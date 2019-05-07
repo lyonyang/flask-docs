94cb22891a876a55532fb27ea3dfb0f54684d5d2
Lyon <lyon.yang@qq.com>
2019-05-07 22:06:58 +0800

    flask-api-docs

--- setup.py
+++ setup.py
@@ -8,25 +8,24 @@ Flask-Docs
 `````
 """
 import sys
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 from setuptools import setup
 
-version='0.1.0'
+version = '0.1.0'
 
 # Hack to prevent stupid TypeError: 'NoneType' object is not callable error on
 # exit of python setup.py test # in multiprocessing/util.py _exit_function when
 # running python setup.py test (see
 # https://github.com/pypa/virtualenv/pull/259)
-try:
-    import multiprocessing
-except ImportError:
-    pass
 
 install_requires = ['Flask']
 
 setup(
-    name='Flask-Docs',
+    name='Flask-API-Docs',
     version=version,
-    url='https://github.com/lyonyang/flask-docs/flask_docs/',
+    url='https://github.com/lyonyang/flask-docs/flask_docs',
     license='BSD',
     author='Lyon Yang',
     author_email='lyon.yang@qq.com',
@@ -35,7 +34,7 @@ setup(
     description='Web API docs for Flask',
     long_description=__doc__,
     packages=[
-        'flask_docs'
+        'flask_api_docs'
     ],
     zip_safe=False,
     install_requires=install_requires,
@@ -43,17 +42,12 @@ setup(
     ],
     platforms='any',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
+        # 'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Programming Language :: Python :: 3.6'
     ]
 )
